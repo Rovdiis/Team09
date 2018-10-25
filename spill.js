9bac1a63711d43ad7dcd37ae2e4f770ac111723e
evenneven <e.tetler@gmail.com>
2018-10-25 14:44:42 +0200

    nyoppdatert skjelett

--- spill.js
+++ spill.js
@@ -1,23 +1,2 @@
 //Her kan vi legge inn selve JS koden så fort den begynner å falle på plass etter testing.
 //Her har vi da spiller
-
-//jQuery kode til navbar
-$(document).ready(function(){
-  responsive_menu = $('.navbar_ul');
-  $('#menu-acces').on('click', function(e) {
-    e.preventDefault();
-    responsive_menu.slideToggle();
-  });
-  $(window).resize(function(){
-    var obtener_ancho = $(this).width();
-    if(obtener_ancho > 480 && responsive_menu.is(':hidden')) {
-      responsive_menu.removeAttr('style');
-    }
-  });
-  $('nav li').on('click', function(e) {
-    var obtener_ancho = $(window).width();
-    if(obtener_ancho < 480 ) {
-      responsive_menu.slideToggle();
-    }
-  });
-});
