c48e58f799288a29b31997e035740c73132114c3
Jarl Øystein Røvde <jarloysteinrovde@dhcp-10-22-156-175.wlan.ntnu.no>
2018-10-30 13:02:10 +0100

    Fungerende animasjon, bildene ligger linket til min maskin, skal endres

--- spill.js
+++ spill.js
@@ -37,21 +37,25 @@ let points = 0;
 let i = 0;
 let currentChar = null;
 let rightWord = false;
-let progress = document.getElementById("progress");
 let numberOfLetters = 0;
 let stadie1 = document.getElementById("stadie1");
 let stadie2 = document.getElementById("stadie2");
 let stadie3 = document.getElementById("stadie3");
+let theWorm = document.getElementById("theWorm");
+let pos = 100;
 let canvasWidth = 400;
 let canvasHeight = 550;
-
+// let spriteWidth = 125;
+// let spriteHeight = 62.5;
+// let rows = 1;
+// let cols = 2;
+// let width = spriteWidth / cols;
+// let height = spriteHeight / rows;
 let canvas = document.getElementById("spill1");
-let doAnimation = true;
-let worm1 = new Image();
-
-
-
-
+// let doAnimation = true;
+// let worm1 = new Image();
+// let frameCount = 2;
+// let speed = -12;
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
@@ -68,53 +72,21 @@ function player() {
 
 }
 
-function animation(){
 
-  let spriteWidth = 125;
-  let spriteHeight = 62.5;
-  let rows = 1;
-  let cols = 2;
-  let width = spriteWidth / cols;
-  let height = spriteHeight / rows;
-  let curFrame = 0;
-  let frameCount = 2;
-  let x = 180;
-  let y = 0;
-  let srcX;
-  let srcY;
-  let speed = -12;
-  //let worm = [];
-
-let ctx = canvas.getContext("2d");
-    worm1.onload = function() {
-
-      function draw() {
-        updateFrame();
-        ctx.drawImage(worm1, srcX, srcY, width, height, x, y, width, height);
-      }
-
-      function updateFrame() {
-        curFrame = ++curFrame % frameCount;
-        srcX = curFrame * width;
-        ctx.clearRect(x, y, width, height);
-
-        srcY = 0 * height;
-        y -= speed;
-      }
-
-
-
-    setInterval(draw, 500);
-
-    if(!doAnimation){
-      ctx.clearRect(worm1, srcX, srcY, width, height, x, y, width, height);
-      ctx.clearRect(x, y, width, height);
-      return;
+animate();
+function animate() {
+  var theWorm = document.getElementById("theWorm");
+  var id = setInterval(frame, 50);
+  function frame() {
+    if (pos == 430) {
+      clearInterval(id);
+    } else {
+      pos++;
+      theWorm.style.top = pos + 'px';
     }
-
-   };
-  worm1.src = "/Users/jarloysteinrovde/Downloads/sprite-animation/spritetest2.png";
+  }
 }
+
 
 
 
@@ -145,7 +117,7 @@ function splitWord() {
 function newWord() {
   randomItem = list[Math.floor(Math.random() * list.length)];
   splitWord();
-  animation();
+  //animation();
   currentChar = letters[i];
 }
 //Denne funksjonen sjekker om det du taster på tastaturet er det samme som neste bokstav
@@ -166,32 +138,32 @@ function checkValue() {
       document.getElementById("word").innerHTML =
         "The word is: " + letters.splice(i).join("");
 
-      // if (numberOfLetters * 0.3 >= letters.length) {
-      //   worm1.style.opacity = "0";
-      //   worm1.style.filter = "alpha(opacity=100)";
-      //   stadie3.style.opacity = "0";
-      //   stadie3.style.filter = "alpha(opacity=0)";
-      // } else if (
-      //   numberOfLetters * 0.3 <= letters.length &&
-      //   numberOfLetters * 0.7 >= letters.length
-      // ) {
-      //   stadie1.style.opacity = "0";
-      //   stadie1.style.filter = "alpha(opacity=0)";
-      //   stadie2.style.opacity = "1";
-      //   stadie2.style.filter = "alpha(opacity=100)";
-      // } else {
-      //   stadie2.style.opacity = "0";
-      //   stadie2.style.filter = "alpha(opacity=0)";
-      //   stadie3.style.opacity = "1";
-      //   stadie3.style.filter = "alpha(opacity=100)";
-      // }
+      if (numberOfLetters * 0.3 >= letters.length) {
+        stadie1.style.opacity = "1";
+        stadie1.style.filter = "alpha(opacity=100)";
+        stadie3.style.opacity = "0";
+        stadie3.style.filter = "alpha(opacity=0)";
+      } else if (
+        numberOfLetters * 0.3 <= letters.length &&
+        numberOfLetters * 0.7 >= letters.length
+      ) {
+        stadie1.style.opacity = "0";
+        stadie1.style.filter = "alpha(opacity=0)";
+        stadie2.style.opacity = "1";
+        stadie2.style.filter = "alpha(opacity=100)";
+      } else {
+        stadie2.style.opacity = "0";
+        stadie2.style.filter = "alpha(opacity=0)";
+        stadie3.style.opacity = "1";
+        stadie3.style.filter = "alpha(opacity=100)";
+      }
     } else {
       rightWord = true; //jepp, sett til null
       document.getElementById("word").innerHTML =
         "The word is: " + letters.splice(i).join("");
       //Legger til et poeng
       points++;
-      resetCanvas()
+      //resetCanvas()
 
     }
   } else {
@@ -208,11 +180,10 @@ function goAgain() {
   currentChar = null;
   rightWord = false;
   newWord();
-  animation();
   //doAnimation = true;
-  // stadie3.style.opacity = "0";
-  // stadie3.style.filter = "alpha(opacity=0)";
+  stadie3.style.opacity = "0";
+  stadie3.style.filter = "alpha(opacity=0)";
+  stadie1.style.opacity = "1";
+  stadie1.style.filter = "alpha(opacity=100)";
+  pos = 100;
 }
-function resetCanvas() {
-        doAnimation = false;
-      }
