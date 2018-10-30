6843906b7e3a6f425160a2c03af1ef34cc0b30f8
Jarl Øystein Røvde <jarloysteinrovde@dhcp-10-22-156-175.wlan.ntnu.no>
2018-10-30 15:48:28 +0100

    Etter workshop

--- spill.js
+++ spill.js
@@ -42,6 +42,7 @@ let stadie1 = document.getElementById("stadie1");
 let stadie2 = document.getElementById("stadie2");
 let stadie3 = document.getElementById("stadie3");
 let theWorm = document.getElementById("theWorm");
+let explotion = document.getElementById("explotion");
 let pos = 100;
 let canvasWidth = 400;
 let canvasHeight = 550;
@@ -56,9 +57,7 @@ player();
 function player() {
   base_image = new Image();
   base_image.src = "/Users/jarloysteinrovde/Downloads/sprite-animation/FULLSET3liv.png";
-//  base_image.onload = function() {
     playerctx.drawImage(base_image, 0, 150, 400, 400);
-//  };
 
 }
 
@@ -66,9 +65,9 @@ function player() {
 animate();
 function animate() {
   var theWorm = document.getElementById("theWorm");
-  var id = setInterval(frame, 25);
+  var id = setInterval(frame, 50);
   function frame() {
-    if (pos == 430) {
+    if (pos == 390) {
       clearInterval(id);
       console.log("Game over");
 
@@ -84,9 +83,7 @@ setInterval(player, 0.001);
 
 function play(event) {
   keyPressed = event.keyCode;
-  //console.log(event.keyCode);
   keyPressedChar = String.fromCharCode(keyPressed);
-  //console.log("Keyboard press works: " + keyPressedChar);
   document.getElementById("points").innerHTML = "Din poengsum er: " + points;
 
   splitWord();
@@ -96,37 +93,34 @@ function play(event) {
 function splitWord() {
   letters = randomItem.split("");
   //Denne displayer order på skjermen
-  document.getElementById("word").innerText =
-    "The word is: " + letters.join("");
+  document.getElementById("word").innerText = letters.join("");
   numberOfLetters = letters.length;
-  //console.log(numberOfLetters);
 }
 //Denne funksjonen velger ut et nytt tilfeldig ord
 function newWord() {
   randomItem = list[Math.floor(Math.random() * list.length)];
   splitWord();
-  //animation();
+
   currentChar = letters[i];
 }
 //Denne funksjonen sjekker om det du taster på tastaturet er det samme som neste bokstav
 function checkValue() {
   if (rightWord) {
     //Hvis ordet er riktig
-    //  console.log("Ferdig :D");
+    wormsplotion();
+
     goAgain();
     return;
   }
-  //  console.log("sammenligner: " + currentChar + " og " + keyPressedChar);
   if (keyPressedChar === currentChar) {
-    //  console.log("Du fant riktig bokstav!");
     i++; //Inkrementer teller for å hente neste bokstav
     if (i < letters.length) {
       // Er vi på siste siste bokstav?
       currentChar = letters[i]; //nopp, hent neste
-      document.getElementById("word").innerHTML =
-        "The word is: " + letters.splice(i).join("");
+      document.getElementById("word").innerHTML = letters.splice(i).join("");
 
       if (numberOfLetters * 0.3 >= letters.length) {
+        explotion.innerHTML = " ";
         stadie1.style.opacity = "1";
         stadie1.style.filter = "alpha(opacity=100)";
         stadie3.style.opacity = "0";
@@ -144,22 +138,18 @@ function checkValue() {
         stadie2.style.filter = "alpha(opacity=0)";
         stadie3.style.opacity = "1";
         stadie3.style.filter = "alpha(opacity=100)";
+
       }
     } else {
       rightWord = true; //jepp, sett til null
-      document.getElementById("word").innerHTML =
-        "The word is: " + letters.splice(i).join("");
+      document.getElementById("word").innerHTML = letters.splice(i).join("");
       //Legger til et poeng
       points++;
-      //resetCanvas()
 
     }
   } else {
     //Hvis bruker trykker feil bokstav
-    document.getElementById("word").innerHTML =
-      "The word is: " + letters.splice(i).join("");
-    //console.log("Feil bokstav!!");
-    //console.log("riktig er: " + letters[i]);
+    document.getElementById("word").innerHTML = letters.splice(i).join("");
   }
 }
 //Denne funksjonen kjører det som trengs for at et nytt ord skal kunne dukke opp når man er ferdig med forrige ord.
@@ -168,10 +158,15 @@ function goAgain() {
   currentChar = null;
   rightWord = false;
   newWord();
-  //doAnimation = true;
   stadie3.style.opacity = "0";
   stadie3.style.filter = "alpha(opacity=0)";
   stadie1.style.opacity = "1";
   stadie1.style.filter = "alpha(opacity=100)";
+
   pos = 100;
+}
+
+function wormsplotion(){
+  explotion.innerHTML = "<img src='/Users/jarloysteinrovde/Desktop/bilder_spill/explosion-3.gif'>";
+
 }
